7f2332e5d3947f4e52ca7cd86f7852eac0f85802
carnage <lot_carnage@nifty.com>
2019-06-22 01:38:41 +0900

    Add indexing method and unittest
    - Add indexing
    - Add slice indexing
    - Add unit test
    - Add bool method

--- pyaid/slicer.py
+++ pyaid/slicer.py
@@ -5,30 +5,91 @@ class slicer:
         self.array_like = array_like
         self.stride = stride
         self.width = width
-        self.eatup = eatup
+        self.index = 0
+        if eatup:
+            self.length = ((len(array_like) + stride - 1) // stride)
+        else:
+            self.length = (len(array_like) - width) // stride + 1
+        return None
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.eatup == True:
-            if len(self.array_like) < self.width:
-                raise StopIteration()
-        else:
-            if len(self.array_like) == 0:
-                raise StopIteration()
-        fragment = self.array_like[:self.width]
-        self.array_like = self.array_like[self.stride:]
+        if self.length == self.index:
+            raise StopIteration()
+        head_index = self.stride * self.index
+        tail_index = head_index + self.width
+        fragment = self.array_like[head_index:tail_index]
+        self.index += 1
         return fragment
 
     def __len__(self):
-        return 1
+        return self.length - self.index
 
+    def __bool__(self):
+        return True
+
+    def __getitem__(self, key):
+        key_type = type(key)
+        if key_type is int:
+            if key < 0 or self.length <= key:
+                raise IndexError
+            head_index = self.stride * key
+            tail_index = head_index + self.width
+            fragment = self.array_like[head_index:tail_index]
+            return fragment
+        if key_type is slice:
+            head = 0 if key.start is None else key.start
+            tail = self.length if key.stop is None else key.stop
+            step = 1 if key.step is None else key.step
+            if step == 0:
+                raise ValueError("slice step cannot be zero")
+            if step < 0:
+                head, tail = tail-1, head-1
+            return [
+                self.array_like[self.stride * i:self.stride * i + self.width]
+                for i in range(head, tail, step)
+            ]
 
 if __name__ == "__main__":
-    a = [0,1,2,3,4,5,6,7,8,9]
-    for v in slicer(a, 1,5):
-        print(v)
-
-    print(len(slicer(a, 1,4)))
+    import unittest
+    class TestSlicer(unittest.TestCase):
+        def test_length(self):
+            self.assertEqual(len(slicer([0,1,2,3,4], 2, 3)), 3)
+            self.assertEqual(len(slicer([0,1,2,3,4], 2, 3, False)), 2)
+        def test_indexing(self):
+            instance = slicer([0,1,2,3,4], 2, 3)
+            self.assertEqual(instance[0], [0,1,2])
+            self.assertEqual(instance[1], [2,3,4])
+            self.assertEqual(instance[2], [4])
+            with self.assertRaises(IndexError):
+                instance[-1]
+            with self.assertRaises(IndexError):
+                instance[-1]
+            with self.assertRaises(IndexError):
+                instance[len(instance) + 1]
+        def test_slice_indexing(self):
+            instance = slicer([0,1,2,3,4], 2, 3)
+            self.assertEqual(instance[0:1], [[0,1,2]])
+            self.assertEqual(instance[:1], [[0,1,2]])
+            self.assertEqual(instance[2:3], [[4]])
+            self.assertEqual(instance[2:], [[4]])
+            self.assertEqual(instance[0::2], [[0,1,2],[4]])
+            self.assertEqual(instance[0:1:-1], [[0,1,2]])
+            self.assertEqual(instance[2:3:-1], [[4]])
+            with self.assertRaises(ValueError):
+                instance[0:1:0]
+        def test_iteration(self):
+            instance = slicer([0,1,2,3,4], 2, 3)
+            self.assertEqual(len(instance), 3)
+            self.assertEqual(instance.__next__(), [0,1,2])
+            self.assertEqual(len(instance), 2)
+            self.assertEqual(instance.__next__(), [2,3,4])
+            self.assertEqual(len(instance), 1)
+            self.assertEqual(instance.__next__(), [4])
+            self.assertEqual(len(instance), 0)
+            with self.assertRaises(StopIteration):
+                self.assertEqual(instance.__next__(), [])
+    unittest.main()
     exit()
