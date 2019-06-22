93f0cf9272df487783d25cd3929731247decc282
carnage <lot_carnage@nifty.com>
2019-06-22 15:29:17 +0900

    Change indexing implementation
    - Delegate indexing to list object.
    - Add rewind method.

--- pyaid/slicer.py
+++ pyaid/slicer.py
@@ -6,6 +6,7 @@ class slicer:
         self.stride = stride
         self.width = width
         self.index = 0
+        self.sliced = None
         if eatup:
             self.length = ((len(array_like) + stride - 1) // stride)
         else:
@@ -18,9 +19,12 @@ class slicer:
     def __next__(self):
         if self.length == self.index:
             raise StopIteration()
-        head_index = self.stride * self.index
-        tail_index = head_index + self.width
-        fragment = self.array_like[head_index:tail_index]
+        if self.sliced is not None:
+            fragment = self.sliced[self.index]
+        else:
+            head_index = self.stride * self.index
+            tail_index = head_index + self.width
+            fragment = self.array_like[head_index:tail_index]
         self.index += 1
         return fragment
 
@@ -32,25 +36,17 @@ class slicer:
 
     def __getitem__(self, key):
         key_type = type(key)
-        if key_type is int:
-            if key < 0 or self.length <= key:
-                raise IndexError
-            head_index = self.stride * key
-            tail_index = head_index + self.width
-            fragment = self.array_like[head_index:tail_index]
-            return fragment
-        if key_type is slice:
-            head = 0 if key.start is None else key.start
-            tail = self.length if key.stop is None else key.stop
-            step = 1 if key.step is None else key.step
-            if step == 0:
-                raise ValueError("slice step cannot be zero")
-            if step < 0:
-                head, tail = tail-1, head-1
-            return [
-                self.array_like[self.stride * i:self.stride * i + self.width]
-                for i in range(head, tail, step)
-            ]
+        if key_type in [int, slice]:
+            if self.sliced is None:
+                index = self.index
+                self.sliced = list(self)
+                self.index = index
+            return self.sliced[key]
+        raise KeyError
+
+    def rewind(self):
+        self.index = 0
+        return None
 
 if __name__ == "__main__":
     import unittest
@@ -63,12 +59,13 @@ if __name__ == "__main__":
             self.assertEqual(instance[0], [0,1,2])
             self.assertEqual(instance[1], [2,3,4])
             self.assertEqual(instance[2], [4])
+            self.assertEqual(instance[-1], [4])
+            self.assertEqual(instance[-2], [2,3,4])
             with self.assertRaises(IndexError):
-                instance[-1]
-            with self.assertRaises(IndexError):
-                instance[-1]
-            with self.assertRaises(IndexError):
+                print(len(instance))
                 instance[len(instance) + 1]
+            with self.assertRaises(KeyError):
+                instance["a"]
         def test_slice_indexing(self):
             instance = slicer([0,1,2,3,4], 2, 3)
             self.assertEqual(instance[0:1], [[0,1,2]])
@@ -76,8 +73,6 @@ if __name__ == "__main__":
             self.assertEqual(instance[2:3], [[4]])
             self.assertEqual(instance[2:], [[4]])
             self.assertEqual(instance[0::2], [[0,1,2],[4]])
-            self.assertEqual(instance[0:1:-1], [[0,1,2]])
-            self.assertEqual(instance[2:3:-1], [[4]])
             with self.assertRaises(ValueError):
                 instance[0:1:0]
         def test_iteration(self):
@@ -90,6 +85,10 @@ if __name__ == "__main__":
             self.assertEqual(instance.__next__(), [4])
             self.assertEqual(len(instance), 0)
             with self.assertRaises(StopIteration):
-                self.assertEqual(instance.__next__(), [])
+                instance.__next__()
+            instance.rewind()
+            self.assertEqual(len(instance), 3)
+            self.assertEqual(instance.__next__(), [0,1,2])
+
     unittest.main()
     exit()
